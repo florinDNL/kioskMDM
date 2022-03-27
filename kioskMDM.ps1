32bcca7765284000f83ea8f30c6464a44aa7cc75
Florin-MS <79944491+florinDNL@users.noreply.github.com>
2022-03-28 01:15:33 +0300

    Add files via upload

---
+++ kioskMDM.ps1
@@ -0,0 +1,84 @@
+﻿function PrintCfg{
+    Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+}
+
+function ApplyCfg{
+    $aacsp  = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    Write-Host "`n1) Multi App`n2) Shell Launcher"
+    $type   = $Host.Ui.ReadLine()
+    
+    if ($type -ne 1 -and $type -ne 2){
+        Write-Host "Invalid Input"        
+        ApplyCfg
+    }
+    else{      
+        $Path   = Read-Host "Enter the path to XML File"
+        $XML    = Get-Content -Path $Path
+        $escXML = [System.Security.SecurityElement]::Escape($XML)
+
+        if     ($type -eq 1){
+            $aacsp.Configuration = $escXML
+        }
+        elseif ($type -eq 2){
+            $aacsp.ShellLauncher = $escXML
+        }
+
+        Set-CimInstance -CimInstance $aacsp
+
+        Write-Host "The current configuration is:"
+        Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    }
+}
+
+function ClearCfg{
+    $count = 0
+    $aacsp  = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    if ($aacsp.Configuration){       
+        $aacsp.Configuration = $NULL 
+        Set-CimInstance -CimInstance $aacsp
+        Write-Host "Multi-App Kiosk Configuration cleared"
+        $count++
+    }
+    if ($aacsp.ShellLauncher){
+        $aacsp.ShellLauncher = $NULL
+        Set-CimInstance -CimInstance $aacsp
+        Write-Host "Shell Launcher Configuration cleared"
+        $count++
+    }
+    if ($count -eq 0){
+        Write-Host "No Multi-App or Shell Launcher Configuration found on present machine"
+    }
+}
+
+function ExtractCfg{
+    $count = 0
+    $aacsp  = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    if ($aacsp.Configuration){
+        $XML = [xml]($aacsp.Configuration)
+        Format-XML $XML -indent 4 >> .\extractedMultiApp.xml
+        Write-Host "Found Multi-App Kiosk, saving configuration as extractedMultiApp.xml"
+        $count++              
+    }
+    if ($aacsp.ShellLauncher){
+        $XML = [xml]($aacsp.ShellLauncher)
+        Format-XML $XML -indent 4 >> .\extractedShellLauncher.xml  
+        Write-Host "Found Shell Launcher, saving configuration as extractedShellLauncher.xml"
+        $count++     
+    }
+    if ($count -eq 0){
+        Write-Host "No Multi-App or Shell Launcher Configuration found on present machine"
+    }
+}
+
+
+function Format-XML ([xml]$xml, $indent=2) #from https://devblogs.microsoft.com/powershell/format-xml/
+{
+    $StringWriter = New-Object System.IO.StringWriter
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+    $xmlWriter.Formatting = “indented”
+    $xmlWriter.Indentation = $Indent
+    $xml.WriteContentTo($XmlWriter)
+    $XmlWriter.Flush()
+    $StringWriter.Flush()
+    return $StringWriter.ToString()
+}
