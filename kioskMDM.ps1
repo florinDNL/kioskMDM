4d70e74734db037c572ddd47e706aea7703f70db
Florin-MS <79944491+florinDNL@users.noreply.github.com>
2022-05-22 15:34:03 +0300

    Update kioskMDM.ps1

--- kioskMDM.ps1
+++ kioskMDM.ps1
@@ -1,88 +1,147 @@
-function PrintCfg{
+function PrintCfg
+{
+
     Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    
 }
 
-function ApplyCfg{
+
+function ApplyCfg
+{
+
     $aacsp  = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
     Write-Host "`n1) Multi App`n2) Shell Launcher"
     $type   = $Host.Ui.ReadLine()
     
-    if ($type -ne 1 -and $type -ne 2){
+    if ($type -ne 1 -and $type -ne 2)
+    {
+    
         Write-Host "Invalid Input"        
         ApplyCfg
+        
     }
-    else{
+    else
+    {
 
-        try{                         
+        try
+        {        
+        
             $escXML = Get-XML
+            
         }
-        catch{
+        catch
+        {
+        
             Write-Host "`n"$_ -ForeGroundColor DarkRed
             break
+            
         }
 
-        if     ($type -eq 1){
+        if     ($type -eq 1)
+        {
+        
             $aacsp.Configuration = $escXML
+            
         }
-        elseif ($type -eq 2){
+        elseif ($type -eq 2)
+        {
+        
             $prodname = (Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").ProductName
-            if ($prodname -like "*Enterprise*" -or $prodname -like "*Education*"){
-                $aacsp.ShellLauncher = $escXML                
+            if ($prodname -like "*Enterprise*" -or $prodname -like "*Education*")
+            {
+            
+                $aacsp.ShellLauncher = $escXML     
+                
             }
-            else{                  
+            else
+            {         
+            
                 Write-Host "Only Windows 10 Enterprise or Education are licensed to use Shell Launcher. You are currently using " $prodname -ForegroundColor DarkRed
                 break
+                
             }
+            
         }        
         
         Set-CimInstance -CimInstance $aacsp
             
         Write-Host "`nThe current configuration is:"
-        Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+        PrintCfg
     }    
+    
 }
 
-function ClearCfg{
+
+function ClearCfg
+{
+
     $count = 0
     $aacsp  = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
-    if ($aacsp.Configuration){       
+    if ($aacsp.Configuration)
+    {       
+    
         $aacsp.Configuration = $NULL 
         Set-CimInstance -CimInstance $aacsp
         Write-Host "Multi-App Kiosk Configuration cleared"
         $count++
+        
     }
-    if ($aacsp.ShellLauncher){
+    if ($aacsp.ShellLauncher)
+    {
+    
         $aacsp.ShellLauncher = $NULL
         Set-CimInstance -CimInstance $aacsp
         Write-Host "Shell Launcher Configuration cleared"
         $count++
+        
     }
-    if ($count -eq 0){
+    if ($count -eq 0)
+    {
+    
         Write-Host "No Multi-App or Shell Launcher Configuration found on present machine"
+        
     }
+
 }
 
-function ExtractCfg{
+
+function ExtractCfg
+{
+
     $count = 0
     $aacsp  = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
-    if ($aacsp.Configuration){
+    
+    if ($aacsp.Configuration)
+    {
+    
         $XML = [xml]($aacsp.Configuration)
         Format-XML $XML -indent 4 >> .\extractedMultiApp.xml
         Write-Host "Found Multi-App Kiosk, saving configuration as extractedMultiApp.xml"
-        $count++              
+        $count++    
+        
     }
-    if ($aacsp.ShellLauncher){
+    
+    if ($aacsp.ShellLauncher)
+    {
+    
         $XML = [xml]($aacsp.ShellLauncher)
         Format-XML $XML -indent 4 >> .\extractedShellLauncher.xml  
         Write-Host "Found Shell Launcher, saving configuration as extractedShellLauncher.xml"
         $count++     
+        
     }
-    if ($count -eq 0){
+    
+    if ($count -eq 0)
+    {
+    
         Write-Host "No Multi-App or Shell Launcher Configuration found on present machine"
+        
     }
 }
 
-function Get-XML{
+
+function Get-XML
+{
     $Path   = Read-Host "Enter the path to XML File"
     $XML    = Get-Content -Path $Path -ErrorAction Stop
     $escXML = [System.Security.SecurityElement]::Escape($XML) 
@@ -90,8 +149,10 @@ function Get-XML{
     return $escXML
 }
 
+
 function Format-XML ([xml]$xml, $indent=2) #from https://devblogs.microsoft.com/powershell/format-xml/
 {
+
     $StringWriter = New-Object System.IO.StringWriter
     $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
     $xmlWriter.Formatting = “indented”
@@ -100,4 +161,5 @@ function Format-XML ([xml]$xml, $indent=2) #from https://devblogs.microsoft.com/
     $XmlWriter.Flush()
     $StringWriter.Flush()
     return $StringWriter.ToString()
+    
 }
